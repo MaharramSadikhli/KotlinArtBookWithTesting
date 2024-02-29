030f183f4ff8029bc8fd4964aa248d034ba960ee
Muarem <s.muarem10@gmail.com>
2024-02-29 15:53:13 +0400

    gradle implementations completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.navigation.safeargs.kotlin)
 }
 
 android {
@@ -27,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -45,4 +48,59 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.jetbrains.kotlin.stdlib)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler.dagger)
+    ksp(libs.hilt.compiler.hilt)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.extensions)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.runtime)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.ktx)
+
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
+    implementation(libs.room.ktx)
+    ksp(libs.kotlinx.metadata.jvm)
+
+    implementation(libs.retrofit2.retrofit)
+    implementation(libs.retrofit2.converter.gson)
+
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
+
+    implementation(libs.bumptech.glide.glide)
+    ksp(libs.bumptech.glide.ksp)
+
+    implementation(platform(libs.kotlin.bom))
+
+    // test implementation
+    implementation(libs.test.core)
+    testImplementation(libs.hamcrest)
+    testImplementation(libs.core.core.testing)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.truth)
+    testImplementation(libs.mockito.core)
+
+    // android test implementation
+    androidTestImplementation(libs.mockito.android)
+    androidTestImplementation(libs.mockito.core)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
+    androidTestImplementation(libs.core.core.testing)
+    androidTestImplementation(libs.truth)
+    androidTestImplementation(libs.hilt.android.testing)
+    kspAndroidTest(libs.hilt.android.compiler)
+    debugImplementation(libs.fragment.testing)
+
+    androidTestImplementation(libs.espresso.contrib) {
+        exclude(group = "org.checkerframework", module = "checker")
+    }
+
 }
