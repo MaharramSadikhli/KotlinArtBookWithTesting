afd2a40eb6d52d5e5380755e26842827927cfed2
Muarem <s.muarem10@gmail.com>
2024-03-01 17:41:28 +0400

    created pixabay result

---
+++ app/src/main/java/com/imsosoft/kotlinartbookwithtesting/util/Utils.kt
@@ -0,0 +1,8 @@
+package com.imsosoft.kotlinartbookwithtesting.util
+
+object Utils {
+
+    const val API_KEY = "42645941-2b663d98f8f63de61825819b0"
+    const val BASE_URL = "https://pixabay.com"
+
+}
