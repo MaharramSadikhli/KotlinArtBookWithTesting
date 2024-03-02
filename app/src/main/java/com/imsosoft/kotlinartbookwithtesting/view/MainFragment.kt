6b034696164f642420d240d0e91fb502de3454fe
Muarem <s.muarem10@gmail.com>
2024-03-02 17:40:32 +0400

    created subscribe observer func

--- app/src/main/java/com/imsosoft/kotlinartbookwithtesting/view/MainFragment.kt
+++ app/src/main/java/com/imsosoft/kotlinartbookwithtesting/view/MainFragment.kt
@@ -3,24 +3,48 @@ package com.imsosoft.kotlinartbookwithtesting.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.imsosoft.kotlinartbookwithtesting.R
+import com.imsosoft.kotlinartbookwithtesting.adapter.ArtAdapter
 import com.imsosoft.kotlinartbookwithtesting.databinding.FragmentMainBinding
+import com.imsosoft.kotlinartbookwithtesting.viewmodel.ArtViewModel
+import javax.inject.Inject
 
-class MainFragment: Fragment(R.layout.fragment_main) {
+class MainFragment @Inject constructor(
+    private val artAdapter: ArtAdapter
+): Fragment(R.layout.fragment_main) {
 
     private lateinit var binding: FragmentMainBinding
+    private lateinit var viewModel: ArtViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(requireActivity())[ArtViewModel::class.java]
+
         binding = FragmentMainBinding.bind(view)
+
+        binding.recyclerViewMain.adapter = artAdapter
+        binding.recyclerViewMain.layoutManager = LinearLayoutManager(requireContext())
+
 
         binding.fab.setOnClickListener {
             val action = MainFragmentDirections.actionMainFragmentToDetailsFragment()
             Navigation.findNavController(it).navigate(action)
         }
 
+        subscribeToObservers()
+
     }
+
+    private fun subscribeToObservers() {
+        viewModel.artList.observe(viewLifecycleOwner, Observer {
+            artAdapter.arts = it
+        })
+    }
+
 
 }
