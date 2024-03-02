d9c0951d877c48917874a9f0257a5a525a87cefd
Muarem <s.muarem10@gmail.com>
2024-03-02 17:43:02 +0400

    created swipe callback

--- app/src/main/java/com/imsosoft/kotlinartbookwithtesting/view/MainFragment.kt
+++ app/src/main/java/com/imsosoft/kotlinartbookwithtesting/view/MainFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.imsosoft.kotlinartbookwithtesting.R
 import com.imsosoft.kotlinartbookwithtesting.adapter.ArtAdapter
 import com.imsosoft.kotlinartbookwithtesting.databinding.FragmentMainBinding
@@ -20,6 +22,23 @@ class MainFragment @Inject constructor(
     private lateinit var binding: FragmentMainBinding
     private lateinit var viewModel: ArtViewModel
 
+    private val swipeCallBack = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val layoutPosition = viewHolder.layoutPosition
+            val selectedArt = artAdapter.arts[layoutPosition]
+            viewModel.deleteArt(selectedArt)
+        }
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -30,6 +49,7 @@ class MainFragment @Inject constructor(
         binding.recyclerViewMain.adapter = artAdapter
         binding.recyclerViewMain.layoutManager = LinearLayoutManager(requireContext())
 
+        ItemTouchHelper(swipeCallBack).attachToRecyclerView(binding.recyclerViewMain)
 
         binding.fab.setOnClickListener {
             val action = MainFragmentDirections.actionMainFragmentToDetailsFragment()
