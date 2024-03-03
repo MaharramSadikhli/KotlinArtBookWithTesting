c2f7a13a35d3a413c767a348d3cfb293c44cd4fe
Muarem <s.muarem10@gmail.com>
2024-03-03 21:21:27 +0400

    completed room db testing

--- app/src/androidTest/java/com/imsosoft/kotlinartbookwithtesting/roomdb/ArtDaoTest.kt
+++ app/src/androidTest/java/com/imsosoft/kotlinartbookwithtesting/roomdb/ArtDaoTest.kt
@@ -1,33 +1,42 @@
 package com.imsosoft.kotlinartbookwithtesting.roomdb
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth
+import com.imsosoft.kotlinartbookwithtesting.getOrAwaitValueForAndroidTest
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
+import javax.inject.Named
 
 
 @SmallTest
 @ExperimentalCoroutinesApi
+@HiltAndroidTest
 class ArtDaoTest {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var artDb: ArtDB
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    @Named("testDb")
+    lateinit var artDb: ArtDB
+
     private lateinit var artDao: ArtDao
 
     @Before
     fun setup() {
 
-        artDb = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(), ArtDB::class.java
-        ).allowMainThreadQueries().build()
+        hiltRule.inject()
 
         artDao = artDb.artDao()
 
@@ -40,13 +49,30 @@ class ArtDaoTest {
 
 
     @Test
-    fun `insert list of arts`() = runTest {
+    fun testForInsertArt() = runTest {
 
         val art = ArtEntity("John John", "Da Vinci", 1700, "test.com", 1)
         artDao.insertArt(art)
 
         val listLiveData = artDao.observeArts()
-//        val list = listLiveData
+        val list = listLiveData.getOrAwaitValueForAndroidTest()
+
+        Truth.assertThat(list).contains(art)
+
+    }
+
+
+    @Test
+    fun testForDeleteArt() = runTest {
+
+        val art = ArtEntity("John John", "Da Vinci", 1700, "test.com", 1)
+        artDao.insertArt(art)
+        artDao.deleteArt(art)
+
+        val listLiveData = artDao.observeArts()
+        val list = listLiveData.getOrAwaitValueForAndroidTest()
+
+        Truth.assertThat(list).doesNotContain(art)
 
     }
 
