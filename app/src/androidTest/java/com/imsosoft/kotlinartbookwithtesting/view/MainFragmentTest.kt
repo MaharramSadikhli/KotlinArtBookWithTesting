c21ba4e091aa1880966a742c4ede9abe8b9c105d
Muarem <s.muarem10@gmail.com>
2024-03-05 20:53:29 +0400

    completed main fragment test

--- app/src/androidTest/java/com/imsosoft/kotlinartbookwithtesting/view/MainFragmentTest.kt
+++ app/src/androidTest/java/com/imsosoft/kotlinartbookwithtesting/view/MainFragmentTest.kt
@@ -1,9 +1,16 @@
 package com.imsosoft.kotlinartbookwithtesting.view
 
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.filters.MediumTest
+import com.imsosoft.kotlinartbookwithtesting.R
+import com.imsosoft.kotlinartbookwithtesting.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -33,6 +40,14 @@ class MainFragmentTest {
     fun testFabButtonForNavigationToDetailFragment() {
         val navController = Mockito.mock(NavController::class.java)
 
+        launchFragmentInHiltContainer<MainFragment>(factory = fragmentFactory) {
+            Navigation.setViewNavController(requireView(), navController)
+        }
+
+        Espresso.onView(ViewMatchers.withId(R.id.fab)).perform(ViewActions.click())
+        Mockito.verify(navController).navigate(
+            MainFragmentDirections.actionMainFragmentToDetailsFragment()
+        )
 
     }
 
